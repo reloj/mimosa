652a9190ea71458adeb94d4c164d19c21049b970
Alex Ati <alejandro@atienzaramos.com>
2019-08-04 10:30:59 +0200

    TEMPORAL

--- mimosa.clj
+++ mimosa.clj
@@ -239,166 +239,152 @@
   (is (= (run* [q] (apply-valueo 'x '() q))
          '(x))))
 
-
-(define applyo
-  (lambda (predicate pattern-assoc params fresh-next fresh-next-out out)
-    (conde
-      ((fresh (builtin s1 s2 o1 o2)
-         (conde
-          ((== 'disj builtin))
-          ((== 'conj builtin))
-          ((== '== builtin)))
-         (== `(,builtin ,s1 ,s2) predicate)
-         (== `(,builtin ,o1 ,o2) out)
-         (apply-valueo s1 params o1)
-         (apply-valueo s2 params o2)
-         (== fresh-next fresh-next-out)))
-      ((fresh (fvar fparam fpredicate new-params new-fresh-next)
-         (== `(fresh ,fparam ,fpredicate) predicate)
-         (varo* fresh-next fvar)
-         (ext-so fparam fvar params new-params)
-         (== `(fresh . ,fresh-next) new-fresh-next)
-         (applyo fpredicate pattern-assoc new-params new-fresh-next
-                 fresh-next-out out)))
-      ((fresh (selbri args pattern pv pp a-params combined-params)
-         (== `(,selbri . ,args) predicate)
-         (=/= '== selbri)
-         (=/= 'disj selbri)
-         (=/= 'conj selbri)
-         (=/= 'fresh selbri)
-         (lookupo selbri pattern-assoc pattern)
-         (patterno pv pp pattern)
-         (predicateo pp pattern-assoc)
-         (zipo pv args a-params)
-         (appendo a-params params combined-params)
-         (applyo pp pattern-assoc combined-params fresh-next fresh-next-out out))))))
-
-(check-expect
-  (run* (q) (fresh (fresh-next-out out)
-              (applyo '(== 1 2) '() '() '(fresh) fresh-next-out out)
-              (== `(,out ,fresh-next-out) q)))
-  '(((== 1 2) (fresh))))
-
-(check-expect
-  (run* (q) (fresh (fresh-next-out out)
-              (applyo '(fresh (param x) (== (param x) 1)) '()
-                      '(((param x) 2)) '(fresh) fresh-next-out out)
-              (== `(,out ,fresh-next-out) q)))
-  '(((== (variable (fresh)) 1) (fresh fresh))))
-
-(define predicateo
-  (lambda (p pattern-assoc)
-    (conde
-     ((fresh (s1 s2)
-        (== `(== ,s1 ,s2) p)))
-     ((fresh (selbri s1 s2)
-        (== `(,selbri ,s1 ,s2) p)
-        (conde
-          ((== 'disj selbri))
-          ((== 'conj selbri)))
-        (conde
-          ((varo s1))
-          ((predicateo s1 pattern-assoc)))
-        (conde
-          ((varo s2))
-          ((predicateo s2 pattern-assoc)))))
-     ((fresh (selbri args pattern pv pp a-params)
-        (== `(,selbri . ,args) p)
-        (=/= selbri '==)
-        (=/= selbri 'conj)
-        (=/= selbri 'disj)
-        (=/= selbri 'fresh)
-        (lookupo selbri pattern-assoc pattern)
-        (patterno pv pp pattern)
-        (predicateo pp pattern-assoc)
-        (zipo pv args a-params))))))
-
-(check-expect
- (run* (q) (predicateo '(== 1 1) '()))
- '(_.0))
-
-(define predicatifyo
-  (lambda (s pattern-assoc parent fresh-next fresh-next-out substitution)
-    (fresh (tmp predicate)
+(defn predicateo [p pattern-assoc]
+  (conde
+   [(fresh [s1 s2]
+      (== (list '== s1 s2) p))]
+   [(fresh [verb s1 s2]
+      (== (list verb s1 s2) p)
       (conde
-        ((varo s)
-         (unifyo predicate s tmp substitution))
-        ((not-varo s)
-         (== predicate s)
-         (== tmp substitution)))
-      (predicateo predicate pattern-assoc)
-      (runo* predicate pattern-assoc parent
-             fresh-next fresh-next-out tmp))))
+        [(== 'disj verb)]
+        [(== 'conj verb)]
+        [(varo s1)]
+        [(predicateo s1 pattern-assoc)]
+        [(varo s2)]
+        [(predicateo s2 pattern-assoc)]))]
+   [(fresh [verb args pattern pv pp a-params]
+      (conso verb args p)
+      (!= verb '==)
+      (!= verb 'conj)
+      (!= verb 'disj)
+      (!= verb 'fresh)
+      (lookupo verb pattern-assoc pattern)
+      (patterno pv pp pattern)
+      (predicateo pp pattern-assoc)
+      (zipo pv args a-params))]))
 
-(define runo*
-  (lambda (predicate pattern-assoc parent fresh-next fresh-next-out
-                     out)
+(deftest test-predicateo
+ (is (= (run* [q] (predicateo '(== 1 1) '()))
+        '(_0))))
+
+(declare runo*)
+(defn predicatifyo [s pattern-assoc parent fresh-next fresh-next-out substitution]
+  (fresh [tmp predicate]
     (conde
-      ((fresh (s1 s2)
-         (== `(== ,s1 ,s2) predicate)
-         (unifyo s1 s2 parent out)))
-      ((fresh (s1 s2 out1 out2 fresh-next-out1 fresh-next-out2)
-         (== `(disj ,s1 ,s2) predicate)
+      [(varo s)
+       (unifyo predicate s tmp substitution)]
+      [(not-varo s)
+       (== predicate s)
+       (== tmp substitution)])
+    (predicateo predicate pattern-assoc)
+    (runo* predicate pattern-assoc parent
+           fresh-next fresh-next-out tmp)))
+
+(defn applyo [predicate pattern-assoc params fresh-next fresh-next-out out]
+  (conde
+    [(fresh [builtin s1 s2 o1 o2]
+       (membero builtin ['== 'disj 'conj])
+       (== (list builtin s1 s2) predicate)
+       (== (list builtin o1 o2) out)
+       (apply-valueo s1 params o1)
+       (apply-valueo s2 params o2)
+       (== fresh-next fresh-next-out))]
+    [(fresh [fvar fparam fpredicate new-params new-fresh-next]
+       (== (list 'fresh fparam fpredicate) predicate)
+       (varo* fresh-next fvar)
+       (conso [fparam fvar] params new-params)
+       (conso 'fresh fresh-next new-fresh-next)
+       (applyo fpredicate pattern-assoc new-params new-fresh-next
+               fresh-next-out out))]
+    [(fresh [verb args pattern pv pp a-params combined-params]
+       (== (list verb args) predicate)
+       (!= '== verb)
+       (!= 'disj verb)
+       (!= 'conj verb)
+       (!= 'fresh verb)
+       (lookupo verb pattern-assoc pattern)
+       (patterno pv pp pattern)
+       (predicateo pp pattern-assoc)
+       (zipo pv args a-params)
+       (appendo a-params params combined-params)
+       (applyo pp pattern-assoc combined-params fresh-next fresh-next-out out))]))
+
+(deftest test-applyo
+  (is (= (run* [q] (fresh [fresh-next-out out]
+                     (applyo '(== 1 2) '() '() '(fresh) fresh-next-out out)
+                     (== (list out fresh-next-out) q)))
+         '(((== 1 2) (fresh)))))
+  (is (= (run* [q] (fresh [fresh-next-out out]
+                     (applyo '(fresh [:param x] (== [:param x] 1)) '()
+                             '(([:param x] 2)) '(fresh) fresh-next-out out)
+                     (== `(~out ~fresh-next-out) q)))
+         '(((== (:variable (fresh)) 1) (fresh fresh))))))
+
+(defn runo* [predicate pattern-assoc parent fresh-next fresh-next-out
+                     out]
+    (conde
+      [(fresh [s1 s2]
+         (== (list '== s1 s2) predicate)
+         (unifyo s1 s2 parent out))]
+      [(fresh [s1 s2 out1 out2 fresh-next-out1 fresh-next-out2]
+         (== (list 'disj s1 s2) predicate)
          (predicatifyo s1 pattern-assoc parent fresh-next
                           fresh-next-out1 out1)
          (predicatifyo s2 pattern-assoc parent fresh-next
                           fresh-next-out2 out2)
          (conde
-           ((== out1 #f) (== out2 #f)
-                       (== out #f) (== fresh-next-out fresh-next))
-           ((=/= out1 #f) (== out2 #f)
-                        (== out out1) (== fresh-next-out fresh-next-out1))
-           ((== out1 #f) (=/= out2 #f)
-                       (== out out2) (== fresh-next-out fresh-next-out2))
-           ((=/= out1 #f) (=/= out2 #f)
+           [(== out1 fail) (== out2 fail)
+                       (== out fail) (== fresh-next-out fresh-next)]
+           [(!= out1 fail) (== out2 fail)
+                        (== out out1) (== fresh-next-out fresh-next-out1)]
+           [(== out1 fail) (!= out2 fail)
+                       (== out out2) (== fresh-next-out fresh-next-out2)]
+           [(!= out1 fail) (!= out2 fail)
                         (conde ((== out out1) (== fresh-next-out fresh-next-out1))
-                               ((== out out2) (== fresh-next-out fresh-next-out2)))))))
-      ((fresh (s1 s2 o1 o2 fresh-next-tmp tmp)
-         (== `(conj ,s1 ,s2) predicate)
+                               ((== out out2) (== fresh-next-out fresh-next-out2)))]))]
+      [(fresh [s1 s2 o1 o2 fresh-next-tmp tmp]
+         (== (list 'conj s1 s2) predicate)
          (predicatifyo s1 pattern-assoc parent fresh-next
                        fresh-next-tmp tmp)
          (predicatifyo s2 pattern-assoc tmp fresh-next-tmp
-                       fresh-next-out out)))
-      ((fresh (selbri rest fresh-next-tmp applied)
-         (== `(,selbri . ,rest) predicate)
-         (=/= '== selbri)
-         (=/= 'conj selbri)
-         (=/= 'disj selbri)
+                       fresh-next-out out))]
+      [(fresh [verb rest fresh-next-tmp applied]
+         (conso verb rest predicate)
+         (!= '== verb)
+         (!= 'conj verb)
+         (!= 'disj verb)
          (applyo predicate pattern-assoc '() fresh-next
                  fresh-next-tmp applied)
          (runo* applied pattern-assoc parent fresh-next-tmp
-                fresh-next-out out))))))
+                fresh-next-out out))]))
 
-(define selecto*
-  (lambda (queries s ss)
+(declare selecto)
+(defn selecto* [queries s ss]
     (conde
-      ((== '() queries) (== '() ss))
-      ((fresh (qf qr ssf ssr)
-         (== `(,qf . ,qr) queries)
-         (== `(,ssf . ,ssr) ss)
+      [(== '() queries) (== '() ss)]
+      [(fresh [qf qr ssf ssr]
+         (conso qf qr queries)
+         (conso ssf ssr ss)
          (selecto qr s ssr)
-         (walko qf s ssf))))))
+         (walko qf s ssf))]))
 
-(define selecto
-  (lambda (queries s ss)
+(defn selecto [queries s ss]
     (conde
-      ((== s #f) (== ss #f))
-      ((=/= s #f) (selecto* queries s ss)))))
+      [(== s fail) (== ss fail)]
+      [(!= s fail) (selecto* queries s ss)]))
 
-(define runo
-  (lambda (queries predicate pattern-assoc ss)
-    (fresh (s fresh-out)
+(defn runo [queries predicate pattern-assoc ss]
+    (fresh [s fresh-out]
       (runo* predicate pattern-assoc '() '(fresh) fresh-out s)
-      (selecto queries s ss))))
+      (selecto queries s ss)))
 
 ;; (display
 ;;   (run 1 (patterns)
 ;;     (runo '() '(mother a b) patterns '())
 ;;     (runo '() '(mother b c) patterns '())
-;;     (runo '() '(mother a c) patterns #f)
+;;     (runo '() '(mother a c) patterns fail)
 ;;     (runo '() '(grandmother a c) patterns '())
-;;     (runo '() '(grandmother a b) patterns #f)
-;;     (runo '() '(grandmother b c) patterns #f)))
+;;     (runo '() '(grandmother a b) patterns fail)
+;;     (runo '() '(grandmother b c) patterns fail)))
 
-(test)
 
